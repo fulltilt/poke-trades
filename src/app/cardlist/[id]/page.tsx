9b62fcbbbca712724dfdecc3086738386cb909ef
David Doria <daviddoria@Davids-MacBook-Pro-2.local>
2024-07-18 17:07:26 -0700

    started adding info to card ui

--- src/app/cardlist/[id]/page.tsx
+++ src/app/cardlist/[id]/page.tsx
@@ -4,6 +4,26 @@ import SearchInput from "~/components/searchInput";
 import { SkeletonCard } from "~/components/skeletonCard";
 import { getCardsFromSet } from "~/server/queries";
 import type { Card } from "~/server/queries";
+import { auth } from "@clerk/nextjs/server";
+
+function Favorite(fill: string) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="#b6b6b6"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      // stroke="currentColor"
+      className="size-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+      />
+    </svg>
+  );
+}
 
 export default function CardList({
   params,
@@ -13,6 +33,8 @@ export default function CardList({
   searchParams?: { search?: string; page?: string; pageSize?: string };
 }) {
   async function Cards() {
+    const user = auth();
+
     const currentPage = Number(searchParams?.page) ?? 1;
     const pageSize = Number(searchParams?.pageSize) ?? 30;
     const search = searchParams?.search ?? "";
@@ -35,12 +57,19 @@ export default function CardList({
         </div>
         <div className="m-auto grid max-w-[1200px] gap-4 md:grid-cols-4 lg:grid-cols-6">
           {cards.map((card: Card | null) => (
-            <div key={card?.id}>
+            <div key={card?.id} className="pb-8">
               <img
                 src={card?.images.small}
                 alt={`${card?.name}`}
                 className="cursor-pointer transition-all duration-200 hover:scale-105"
               />
+              <div className="flex justify-between">
+                <div>{card?.number}/</div>
+                <div>$</div>
+                <div>
+                  <Favorite fill={"#b6b6b6"} />
+                </div>
+              </div>
             </div>
           ))}
         </div>
